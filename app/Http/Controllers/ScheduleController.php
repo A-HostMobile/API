db7763da9d344e3bed5287af7bc4e077da7c9075
hgcasshipeia <ph.thitikarn_st@tni.ac.th>
2017-05-31 11:16:04 +0700

    Create_Schedule_Controller

---
+++ app/Http/Controllers/ScheduleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ScheduleController extends Controller
+{
+    public function searchSchedule(){
+
+    }
+
+    public function otherSchedule(){
+    	
+    }
+}
