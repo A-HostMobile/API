ce26e56d1168ad202895d7f2f741ebdf40952a0c
hgcasshipeia <ph.thitikarn_st@tni.ac.th>
2017-05-31 16:00:49 +0700

    update_schedule

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -4,14 +4,95 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use DateTime;
+use PDO;
+use DB;
+use App\Schedules as Schedule;
+use App\Helper\CustomHelper as Help;
+use Str;
 
 class ScheduleController extends Controller
 {
-    public function searchSchedule(){
+    public function searchSchedule($dest){
 
+    	try{
+      		 
+      		 // $destination = 'JAKARTA';
+			 $today = date("d/m/Y",time());
+			 $condition_date = " S_CLOSING_DATE >= TO_DATE('$today', 'DD/MM/YYYY')+ INTERVAL '2' DAY ORDER BY S_ETD ASC";
+
+			 $result = DB::select(DB::raw("SELECT S_SCHEDULE_ID,S_TYPE,S_FROM,S_TO,S_CARRIER,S_FEEDER,S_FVOY,S_VESSEL,S_VOY,
+			 							S_CLOSING_DATE,S_ETD,S_ETA,S_FEEDER_FLAG,CREATION_DATE,CREATED_BY,LAST_UPDATE_DATE,
+			 							LAST_UPDATED_BY,(SELECT VALIDATE_CLOSING_DATE(S_CLOSING_DATE) FROM DUAL) AS TIME 
+										FROM SCHEDULES
+										WHERE S_TO= '$dest' 
+										AND $condition_date"));
+
+			 $data = array();
+			 foreach ($result as $row ) 
+				{
+					if($row->time == 1){
+						$row->time = "Monday-Friday before: 17.00 PM";
+					}
+					else if($row->time == 2){
+						$row->time = "Saturday Closing Date  before : 12.00 AM";
+					}
+					
+					$data[] = $row;
+					
+				}
+
+			
+             $resp = Help::createResp(0,"Query Success",$data);
+             return response()->json($resp);
+
+        }catch(\Illuminate\Database\QueryException $e){
+
+            $resp = Help::createResp(2,"Query Error",$e->getMessage());
+            return response()->json($resp);
+
+        }catch(\Exception $e){
+
+            $resp = Help::createResp(1,"Connect DB Error",$e->getMessage());
+            return response()->json($resp);
+        }
     }
 
-    public function otherSchedule(){
-    	
+    public function otherSchedule($dest,$loading){
+
+    	try{
+      		 
+      		 // $destination = 'JAKARTA';
+			 $today = date("d/m/Y",time());
+			 $condition_date = " S_CLOSING_DATE >= TO_DATE('$today', 'DD/MM/YYYY')+ INTERVAL '2' DAY ORDER BY S_ETD ASC";
+
+			 $result = DB::select(DB::raw("SELECT S_SCHEDULE_ID,S_TYPE,S_FROM,S_TO,S_CARRIER,S_FEEDER,S_FVOY,S_VESSEL,S_VOY,
+			 							S_CLOSING_DATE,S_ETD,S_ETA,S_FEEDER_FLAG,CREATION_DATE,CREATED_BY,LAST_UPDATE_DATE,
+			 							LAST_UPDATED_BY,(SELECT VALIDATE_CLOSING_DATE(S_CLOSING_DATE) FROM DUAL) AS TIME 
+										FROM SCHEDULES
+										WHERE S_TO= '$dest' 
+										AND $condition_date"));
+			 $data = array();
+			 foreach ($result as $row ) 
+				{
+					$data[] = $row;
+					
+				}
+
+		
+             $resp = Help::createResp(0,"Query Success",$data);
+             return response()->json($resp);
+
+        }catch(\Illuminate\Database\QueryException $e){
+
+            $resp = Help::createResp(2,"Query Error",$e->getMessage());
+            return response()->json($resp);
+
+        }catch(\Exception $e){
+
+            $resp = Help::createResp(1,"Connect DB Error",$e->getMessage());
+            return response()->json($resp);
+        }
+
     }
 }
